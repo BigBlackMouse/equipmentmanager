23c311b32d16600e5cb86a5d33f0a3f4cad55610
ssm <1290375385@qq.com>
2023-06-02 13:39:04 +0800

    第二次提交

--- src/main/java/com/sicau/springbootequipmentmanager/equipmentmanager/EquipmentManagerApplication.java
+++ src/main/java/com/sicau/springbootequipmentmanager/equipmentmanager/EquipmentManagerApplication.java
@@ -1,13 +1,16 @@
 package com.sicau.springbootequipmentmanager.equipmentmanager;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class EquipmentManagerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EquipmentManagerApplication.class, args);
+        log.info("舞台已搭建，请开始你的表演");
     }
 
 }
