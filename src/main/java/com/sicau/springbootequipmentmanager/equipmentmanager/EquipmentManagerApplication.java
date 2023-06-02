b445cca5e0d09189ff0128a0f2dc20bec1767830
邓守彬 <@qq.com>
2023-06-02 13:43:50 +0800

    这是一个提交测试

--- src/main/java/com/sicau/springbootequipmentmanager/equipmentmanager/EquipmentManagerApplication.java
+++ src/main/java/com/sicau/springbootequipmentmanager/equipmentmanager/EquipmentManagerApplication.java
@@ -10,7 +10,7 @@ public class EquipmentManagerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EquipmentManagerApplication.class, args);
-        log.info("舞台已搭建，请开始你的表演");
+        log.info("舞台已搭建，请开始你的表演:这是一个提交测试");
     }
 
 }
